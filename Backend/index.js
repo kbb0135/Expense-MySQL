9177fd4e548444abd326cbe3e734bd1f3d62bae5
Budhathoki, Karan <budhathokikaran135@gmail.com>
2023-12-31 20:10:26 -0600

    Backend connected to MYSQL database

---
+++ Backend/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express();
+const PORT_NO = 7000;   //port number client will send or fetch data from
+//For the initialiation of the table
+const db= require("./models")   //iterate all the files to check or create tables
+
+//initialize table if not created
+//will return a promise if successful
+
+db.sequelize.sync().then(()=> {
+    //making sure that server or backend will only work when there is a table
+    app.listen(PORT_NO, ()=>{
+        console.log('Server running on port ',PORT_NO);
+    })
+})
+
+// app.listen(PORT_NO, ()=>{
+//     console.log('Server running on port ',PORT_NO);
+// })
---
+++ Backend/models/AddExpense.js
@@ -0,0 +1,28 @@
+
+
+//Here the module exports takes two parameter that helps to initialize the tabel
+//instead of creating table manually 
+//we use sequlize to automatically create the table
+module.exports = (sequelize, DataTypes) => {
+    //the type we define is the name of the table
+    const AddExpense = sequelize.define("AddExpense", {
+        //table columns 
+        expenseName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        category: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        price: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    })
+    return AddExpense;
+}
